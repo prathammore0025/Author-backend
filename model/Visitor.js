40b75405069d6faa09087e3bedce061d46fc219f
Pratham More <156173075+prathammore0025@users.noreply.github.com>
2025-01-11 16:54:10 +0530

    Delete model/Visitor.js

--- model/Visitor.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const VisitorSchema = new mongoose.Schema({
-  timestamp: { type: Date, default: Date.now },
-  accessedAuthor: String,
-});
-
-module.exports = mongoose.model('Visitor', VisitorSchema);
